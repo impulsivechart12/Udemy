6628557d79b6093dfe168104209c30524c1d2a7b
Impulsivechart <impulsivechart@gmail.com>
2023-10-13 19:36:31 -0600

    hangman game coding day 6 complete

--- hangman.py
+++ hangman.py
@@ -1,93 +1,44 @@
 import random
-from typing import List
+from hangman_words import word_list
+from hangman_art import logo, stages
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-word_list = ["apple","pineapple","impulsive","supercar","astroid"]
 chosen_word = random.choice(word_list)
-
+print(logo)
 print(f"Psst, the solution is {chosen_word}")
 
-
+guessed_letter = []
 display = []
-counter = "_"
 word_length = len(chosen_word)
 for number in range(word_length):
     display += "_"
 
 end_game = False
-lives = 7
+lives = 6
 
 while not end_game:
     guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"You've alread guessed {guess}")
 
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
 
-    if letter != guess:
-        lives -= 1
-        if lives == 0:
-            end_game = True
-            print("You Lose!")
+    if guess not in chosen_word:
+        if guess in guessed_letter:
+            print(f"You have alreadly guessed the letter {guess}")
+        else:
+            lives -= 1
+            print(f"You guessed {guess}, that's not in the word. You lose a life\n"
+              f"-----------------------------")
+            if lives == 0:
+                end_game = True
+                print("You Lose!")
 
+    guessed_letter += guess
     print(display)
-
 
     if "_" not in display:
         end_game = True
