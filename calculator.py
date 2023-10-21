3d0237848845beb22d4d6f85a85a2f63d2e48cd0
Impulsivechart <impulsivechart@gmail.com>
2023-10-21 11:42:25 -0600

    Middle of while loop calculator challenge day 10

--- calculator.py
+++ calculator.py
@@ -1,7 +1,5 @@
 from calculator_art import logo
 
-print(logo)
-
 #Addition
 def add(n1, n2):
     return n1 + n2
@@ -25,19 +23,26 @@ operations = {
     "/": divide,
             }
 
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick and operation from the line above: ")
-num2 = int(input("What's the second number?: "))
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    still_calculating = True
 
-operation_symbol = input("Pick another operation: ")
-num3 = int(input("What's the next number?: "))
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(first_answer, num3)
+    while still_calculating:
+        operation_symbol = input("Pick and operation from the line above: ")
+        num2 = float(input("What's the second number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calcualting with {answer}, or type 'n' to to start a new calculation: ").lower() == 'y':
+            num1 = answer
+        else:
+            still_calculating = False
+            calculator()
+
+calculator()
