06dcd0cf9913a7cadf80d508f7895dd7fb005f1c
Impulsivechart <impulsivechart@gmail.com>
2023-12-18 22:18:47 -0700

    Finished day 18, OOP quiz game.

---
+++ quiz_main.py
@@ -0,0 +1,22 @@
+from quiz_question_model import Question
+from quiz_game_data import question_data
+from quiz_brain import QuizBrain
+import random
+
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz.")
+print(f"Your final score was: {quiz.score} out of {quiz.question_number}")
+
