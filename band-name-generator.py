2db38c979098d65396ea91a02cadadd409dacde1
impul <impulsivechart12@gmail.com>
2025-11-23 16:23:56 -0700

    Back at it

--- band-name-generator.py
+++
@@ -1,6 +0,0 @@
-print('Welcome to the Band Name Generator.')
-city = input("What's the name of the city you grew  up in?\n")
-print(city)
-pet_name = input("what's your pet's name?\n")
-print(pet_name)
-print("Your band name could be " + city + " " + pet_name)
