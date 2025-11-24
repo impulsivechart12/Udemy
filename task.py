b38cde3548c90cf308a4495c15c95c6694315a22
impul <impulsivechart12@gmail.com>
2025-11-23 17:29:16 -0700

    Day two completed

--- task.py
+++ task.py
@@ -1,4 +1,6 @@
-name = input("What is your name?")
-lengthofname = len(name)
-print(name)
-print(lengthofname)
+height = 1.65 
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight / height**2
